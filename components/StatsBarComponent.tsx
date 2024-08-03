b63de2b2c23164d8e6a7c3957ad574f9d1e12b25
ramses <ramsestetang2017@outlook.com>
2024-08-03 13:37:52 +0200

    Aufzählung von gelösten Rätseln im der StatsBarComponent

--- components/StatsBarComponent.tsx
+++ components/StatsBarComponent.tsx
@@ -1,12 +1,30 @@
 "use client";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Box, Card, LinearProgress, Stack, Typography} from "@mui/joy";
 import SquareOutlinedIcon from '@mui/icons-material/SquareOutlined';
 import styled from "styled-components";
 import {useMediaQuery} from "@/hooks/useMediaQuery";
+import {useGlobalStore} from "@/store/useGlobalStore";
+import {usePathname, useRouter} from "next/navigation";
+
+const NUMBER_OF_ROOMS = 6  as const;
 
 const StatsBarComponent: React.FC = () => {
     const { isSmall } = useMediaQuery();
+    const pathname = usePathname();
+    const getSpielerFromLocalStorage = useGlobalStore((state) => state.getSpielerFromLocalStorage);
+    const [numberOfChallengesSolved, setNumberOfChallengesSolved] = useState<number>(0);
+
+    useEffect(() => {
+        const player = getSpielerFromLocalStorage();
+        if (!player) return;
+        const resolveNumberOfChallengesSolvedByPlayer = async () => {
+            const playerResults = await useGlobalStore.getState().getErgebnisBySpielerId(player.id);
+            const numberOfChallengesSolvedByPlayer = playerResults.filter((result) => result.geloestIn).length;
+            setNumberOfChallengesSolved(numberOfChallengesSolvedByPlayer);
+        }
+        resolveNumberOfChallengesSolvedByPlayer();
+    }, [pathname]);
 
     return (
         <StatsContainer small={`${isSmall}`}>
@@ -24,7 +42,9 @@ const StatsBarComponent: React.FC = () => {
                             variant="outlined"
                         />
                     </div>
-                    <Typography fontWeight="bold">Angeklickte Objekte: 4%</Typography>
+                    <Typography fontWeight="bold" level={isSmall ? "body-xs" : "body-sm"}>
+                        Angeklickte Objekte: 4%
+                    </Typography>
                 </Stack>
                 <Box component="div">
                     <SquareOutlinedIcon
@@ -38,13 +58,15 @@ const StatsBarComponent: React.FC = () => {
                     <div>
                         <LinearProgress
                             size="lg"
-                            value={20}
+                            value={(numberOfChallengesSolved / NUMBER_OF_ROOMS) * 100}
                             determinate
                             thickness={10}
                             variant="outlined"
                         />
                     </div>
-                    <Typography fontWeight="bold">Gelöste Rätsel: 0 von 6</Typography>
+                    <Typography fontWeight="bold" level={isSmall ? "body-xs" : "body-sm"}>
+                        Gelöste Rätsel: {numberOfChallengesSolved} von 6
+                    </Typography>
                 </Stack>
             </Stack>
         </StatsContainer>
@@ -58,8 +80,9 @@ const StatsContainer = styled(Card)<{ small: "true" | "false" }>`
     transform: translateX(-50%);
     opacity: 70%;
     z-index: var(--z-index-content-behind-modal);
+    min-width: 350px;
 
-    padding: ${(props) => props.small === "true" ? "var(--space-2) var(--space-4)" : "var(--space-2) var(--space-9)"};
+    padding: ${(props) => props.small === "true" ? "var(--space-2) var(--space-3)" : "var(--space-2) var(--space-9)"};
     border-radius: 0 0 var(--space-3) var(--space-3);
     box-shadow: 0 0 10px black;
 

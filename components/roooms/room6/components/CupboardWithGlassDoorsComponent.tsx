c24ca2ffec22b8e3eb933a377effa2972172f6c6
ramses <ramsestetang2017@outlook.com>
2024-08-06 19:57:06 +0200

    Asset ancien_paper.png komprimiert

--- components/roooms/room6/components/CupboardWithGlassDoorsComponent.tsx
+++ components/roooms/room6/components/CupboardWithGlassDoorsComponent.tsx
@@ -18,7 +18,7 @@ export const CupboardWithGlassDoorsComponent: React.FC<InteractiveObjectProps> =
     const modalContent = (
         <Card
             sx={{
-                background: `url('${process.env.NEXT_PUBLIC_BASE_PATH}/rooms/ancien_paper.png')`,
+                background: `url('${process.env.NEXT_PUBLIC_BASE_PATH}/rooms/ancien_paper.jpg')`,
                 backgroundRepeat: "no-repeat",
                 backgroundSize: "cover",
                 width: "inherit",
